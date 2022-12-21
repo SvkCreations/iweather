5edb48bf6c1d94772884e14b649fb0b6ae755071
SvkCreations <thesvk1@gmail.com>
2022-12-21 17:14:53 +0530

    bug fix

--- src/components/WorldForecast.js
+++ src/components/WorldForecast.js
@@ -6,6 +6,7 @@ import eng from '../flags/eng.webp'
 import uae from '../flags/uae.jpg'
 import china from '../flags/china.webp'
 import jpn from '../flags/jap.webp'
+import { Link } from 'react-router-dom';
 
 export default function WorldForecast() {
     return (
